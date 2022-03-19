6ab85dc6977fcd185a46c48c6d987a060752ea34
msamm <msamm93@gmail.com>
2022-03-19 17:12:55 -0500

    initial html and javascript pseudo coding

---
+++ assets/js/quiz.js
@@ -0,0 +1,30 @@
+// Variables needed:
+    // start time
+    // time - for 
+    // timer interval
+    // Questions with answers????
+        // **look up how to create questions
+        // **designate a correct answer
+        // **ensure correct answer is not in the same place
+        // **randomize question order
+        // **don't repeat a question
+    
+
+
+// Listener for start button
+    // On click start time
+
+// Swap out div with question
+// swap out div with answer buttons??
+// Add each answer option as li?? button??
+// Click listener for each option
+// Correct result
+    // Add to score
+    // Show 'correct'
+    // Advance to next question
+        // Swap question again
+        // Swap button content again
+// Incorrect result
+    // Subtract time
+    // Show 'incorrect'
+
